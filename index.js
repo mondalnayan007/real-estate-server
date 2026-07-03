3b348ad216a9f13c54710eed1625f09148142ad7
Nayan Mondal <n007kingkong@gmail.com>
2026-07-04 00:30:31 +0600

    connected the monf=godb

--- index.js
+++ index.js
@@ -3,24 +3,42 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 3000
+const { MongoClient } = require('mongodb');
 
 
 app.use(cors());
 app.use(express.json());
 
 
+const client = new MongoClient(process.env.MONGO_URI);
+async function connectToMongoDB() {
+    try {
+        await client.connect();
 
-app.get('/projects', async(req,res)=>{
-    const cursor = projectsCollection.find();
-    const result = await cursor.toArray();
-    res.send(result) ;
-})
+        app.get('/projects', async (req, res) => {
+            const cursor = projectsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+        console.log("You successfully connected to MongoDB!");
+        return client;
+    } catch (err) {
+        console.dir(err);
+    }
+}
+connectToMongoDB();
+
+
+
+
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
